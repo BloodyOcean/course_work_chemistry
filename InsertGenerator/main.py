2ca059d54b6220410f51ac90c37f6c2d4f1ed953
Oleh Ivsv <you@example.com>
2023-05-03 19:39:36 +0300

    Apply deformation package

--- InsertGenerator/main.py
+++ InsertGenerator/main.py
@@ -1,7 +1,7 @@
 import argparse
-
-from lib.config_helper import ConfigHelper
 from lib.models_generator import ModelsGenerator
+from lib.config_helper import ConfigHelper
+from lib.models_generator import CustomerDeformation
 from models.models import DbHelper
 
 
@@ -9,12 +9,16 @@ def main():
     cfg_helper = ConfigHelper()
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
+
     db_helper = DbHelper(cfg_helper.get('db', 'connection_string'))
     db_helper.connect()
     db_helper.create_tables()
+    
     session = db_helper.session
+
+    deformator = CustomerDeformation(0.5)
     mod_gen = ModelsGenerator(session)
-    res = mod_gen.generate_comment(10)
+    res = mod_gen.generate_customers(10, deformator)
     mod_gen.load_by_lst(res)
 
 
