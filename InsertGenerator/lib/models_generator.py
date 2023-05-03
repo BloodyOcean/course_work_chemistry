b9271fc786dceadce31ea5cdfe0de8fa3a42b2c7
Oleh Ivsv <you@example.com>
2023-05-03 19:39:36 +0300

    Apply deformation package

--- InsertGenerator/lib/models_generator.py
+++ InsertGenerator/lib/models_generator.py
@@ -4,6 +4,7 @@ from typing import List, Type
 from sqlalchemy.orm import Session
 
 from lib.db_service import DbService
+from deformation import *
 from models.models import Customer, ProductCategory, Manufacturer, Order, Payment, Product, OrderItem, Comment
 
 
@@ -23,49 +24,60 @@ class ModelsGenerator:
         self.session = ses
         self.db_service = DbService(self.session)
 
-    def generate_objects(self, count: int, obj_type: Type) -> List[object]:
-        res = []
-        for _ in range(count):
-            obj = obj_type()
-            res.append(obj)
-            if isinstance(obj, Customer):
-                self.customers.append(obj)
-            elif isinstance(obj, ProductCategory):
-                self.categories.append(obj)
-            elif isinstance(obj, Manufacturer):
-                self.manufactors.append(obj)
+   
+    def generate_manufacturer(self, count: int, deformator: ManufacturerDeformationInterface) -> List[Manufacturer]:
+        rows = (Manufacturer() for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
+        self.manufactors.extend(res)
         return res
 
-    def generate_orders(self, count: int) -> List[Order]:
+    def generate_orders(self, count: int, deformator: OrderDeformationInterface) -> List[Order]:
         temp_customers = self.db_service.get_instances(count, Customer)
-        res = [Order(random.choice(temp_customers)) for _ in range(count)]
+        rows = (Order(random.choice(temp_customers)) for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
         self.orders.extend(res)
         return res
 
-    def generate_payments(self, count: int) -> List[Payment]:
+    def generate_categories(self, count: int, deformator:CategoryDeformationInterface) -> List[ProductCategory]:
+        rows = (ProductCategory() for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
+        self.categories.extend(res)
+        return res
+
+    def generate_customers(self, count: int, deformator: CustomerDeformationInterface) -> List[Customer]:
+        rows = (Customer() for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
+        self.customers.extend(res)
+        return res
+
+    def generate_payments(self, count: int, deformator: PaymentDeformationInterface) -> List[Payment]:
         temp_orders = self.db_service.get_instances(count, Order)
-        res = [Payment(random.choice(temp_orders)) for _ in range(count)]
+        rows = (Payment(random.choice(temp_orders)) for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
         self.payments.extend(res)
         return res
 
-    def generate_products(self, count: int) -> List[Product]:
+    def generate_products(self, count: int, deformator: ProductDeformationInterface) -> List[Product]:
         temp_manufactors = self.db_service.get_instances(count, Manufacturer)
         temp_categories = self.db_service.get_instances(5, ProductCategory)
-        res = [Product(random.choice(temp_categories), random.choice(temp_manufactors)) for _ in range(count)]
+        rows = (Product(random.choice(temp_categories), random.choice(temp_manufactors)) for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
         self.products.extend(res)
         return res
 
-    def generate_order_item(self, count: int) -> List[OrderItem]:
+    def generate_order_item(self, count: int, deformator: OrderItemDeformationInterface) -> List[OrderItem]:
         temp_products = self.db_service.get_instances(count, Product)
         temp_orders = self.db_service.get_instances(count, Order)
-        res = [OrderItem(random.choice(temp_products), random.choice(temp_orders)) for _ in range(count)]
+        rows = (OrderItem(random.choice(temp_products), random.choice(temp_orders)) for _ in range(count))
+        res = [deformator.spoil(row) for row in rows]
         self.order_items.extend(res)
         return res
 
-    def generate_comment(self, count: int) -> List[Comment]:
+    def generate_comment(self, count: int, deformator: CommentDeformationInterface) -> List[Comment]:
         temp_products = self.db_service.get_instances(count, Product)
         temp_customers = self.db_service.get_instances(count, Customer)
-        res = [Comment(random.choice(temp_products), random.choice(temp_customers)) for _ in range(count)]
+        rows = (Comment(random.choice(temp_products), random.choice(temp_customers)) for _ in range(count))
+        res = [deformator.spoil(row) for row in rows] 
         self.comments.extend(res)
         return res
 
