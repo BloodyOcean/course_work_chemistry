7abe501accb00ea4bfa1d0afc344c5aed59d69f3
Oleh Ivsv <you@example.com>
2023-05-07 12:50:52 +0300

    Move data from new tables

--- MigrationHelper/main.py
+++ MigrationHelper/main.py
@@ -1,29 +1,33 @@
 from insert_models import *
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import create_engine, MetaData, Table, select, Connection
 from config import CONFIG
 from functools import reduce
 
-def main():
-    levus_is_so_hot_right_now('customers', 'customers', [map_customers])
-    levus_is_so_hot_right_now('product_categories', 'product_categories', [map_product_categorys])
-    levus_is_so_hot_right_now('manufacturers', 'manufacturers', [map_manufacturers])
-    levus_is_so_hot_right_now('products', 'products', [map_products])
-    levus_is_so_hot_right_now('orders', 'orders', [map_orders])
-    levus_is_so_hot_right_now('order_items', 'order_items', [map_order_items])
-    levus_is_so_hot_right_now('comments', 'comments', [map_comments])
-    levus_is_so_hot_right_now('payments', 'payments', [map_payments])
 
-# TODO: rename to 'move_data' after it's not funny anymore
-def levus_is_so_hot_right_now(srcTableName, destTableName, data_processors):
-    '''
+def main():
+    move_data('suppliers', 'suppliers', [map_suppliers])
+    move_data('packaging', 'packaging', [map_packaging])
+    move_data('discounts', 'discounts', [map_discounts])
+    move_data('customers', 'customers', [map_customers])
+    move_data('product_categories', 'product_categories', [map_product_categories])
+    move_data('manufacturers', 'manufacturers', [map_manufacturers])
+    move_data('products', 'products', [map_products])
+    move_data('orders', 'orders', [map_orders])
+    move_data('order_items', 'order_items', [map_order_items])
+    move_data('comments', 'comments', [map_comments])
+    move_data('payments', 'payments', [map_payments])
+    move_data('shipping', 'shipping', [map_shipping])
+
+
+def move_data(srcTableName: str, destTableName: str, data_processors: list) -> None:
+    """
         Moves data from srcTableName to destTableName after getting it through all the processors
 
-        Args:
-            data_processors (array): an array of functions. Each of them receives the result of the previous one
+        :param data_processors is an array of functions. Each of them receives the result of the previous one
         and can do whatever you want. It could be useful if the data should be cleaned before insert.
         For example, you can have a processor to delete some rows, another one to transform some columns,
         and so on.
-    '''
+    """
     src_conn_string = CONFIG['connectionStrings']['sourceConnection']
     dest_conn_string = CONFIG['connectionStrings']['destinationConnection']
 
@@ -42,24 +46,22 @@ def levus_is_so_hot_right_now(srcTableName, destTableName, data_processors):
     src_conn.close()
     dest_conn.close()
 
-# TODO: add a parameter for src connection instead of retrieving mariadb connection string from CONFIG
-def connect_table(tableName, connection_string):
-    '''
+
+def connect_table(tableName: str, connection_string: str) -> (Connection, Table):
+    """
         Connects to the mariadb and creates instances of connection and creates a table based on read metadate
         and 'tableName' parameter
-
-        Returns: tuple(connection, table)
-    '''
+    """
     # create db engine and connect
     db_engine = create_engine(connection_string)
     connection = db_engine.connect()
     # reflect the SQL Server database schema into the MetaData object
     metadata = MetaData()
     metadata.reflect(bind=db_engine)
-    
+
     table = Table(tableName, metadata, autoload=True, autoload_with=db_engine)
-    return (connection, table)
-    
+    return connection, table
+
 
 if __name__ == "__main__":
     main()
