9575c7271b508db19c88b811d13df090e71602f8
danylohlynskyi <danylohlynsky@gmail.com>
2023-05-13 11:10:32 -0400

    Separate DB session creation from the main function

--- InsertGenerator/main.py
+++ InsertGenerator/main.py
@@ -7,6 +7,20 @@ from models.models import DbHelper
 
 
 def main():
+
+    session = get_db_session()
+
+    deformatorDiscount = DiscountDeformation(0.5)
+    deformatorShipping = ShippingDeformation(0.5)
+
+    mod_gen = ModelsGenerator(session)
+    res = mod_gen.generate_discount(100, deformatorDiscount)
+    mod_gen.load_by_lst(res)
+    res = mod_gen.generate_shipping(100, deformatorShipping)
+    mod_gen.load_by_lst(res)
+
+
+def get_db_session():
     # Don't use config.example.ini. Just copy it, rename to config.ini and replace example values 
     # with actual ones. In this way stuff that are used in reality won't be revealed on github.
     cfg_helper = ConfigHelper(path='configs/config.ini')
@@ -17,16 +31,7 @@ def main():
     db_helper.connect()
     db_helper.create_tables()
     
-    session = db_helper.session
-
-    deformatorDiscount = DiscountDeformation(0.5)
-    deformatorShipping = ShippingDeformation(0.5)
-
-    mod_gen = ModelsGenerator(session)
-    res = mod_gen.generate_discount(100, deformatorDiscount)
-    mod_gen.load_by_lst(res)
-    res = mod_gen.generate_shipping(100, deformatorShipping)
-    mod_gen.load_by_lst(res)
+    return db_helper.session
 
 
 if __name__ == '__main__':
